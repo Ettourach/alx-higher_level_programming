e16036e1d96f5c277fc45fa7f3fb60b4b3e3a71c
Ettourach <ettourach@gmail.com>
2024-05-23 17:17:48 -0700

    mysql adv

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,10 @@
+-- Script to displays the average temperature (Fahrenheit) by city ordered by temperature (descending)
+SELECT
+    city,
+    AVG(temperature) AS avg_temp
+FROM
+    temperatures
+GROUP BY
+    city
+ORDER BY
+    avg_temp DESC;
---
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -0,0 +1,10 @@
+-- Script to displays the max temperature of each state (ordered by State name)
+SELECT
+    state,
+    MAX(temperature) AS max_temp
+FROM
+    temperatures
+GROUP BY
+    state
+ORDER BY
+    state;
