e419ff7951be787fc2b620b98fb59ce10c24d3d6
Ettourach <ettourach@gmail.com>
2024-06-18 12:26:45 -0700

    js node

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+// script to prints 3 lines by using an array of string and a loop
+
+let i;
+const myString = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (i = 0; i < 3; i++) {
+  console.log(myString[i]);
+}
